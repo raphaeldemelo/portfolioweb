2c561c17c80cc059c4717ff0efe01e4fce893a32
Raphael de Melo <raphaeldemelosoares@hotmail.com>
2022-03-18 11:12:07 -0300

    criando efeito  scroll reveal

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {
     Container,
     Main,
-    Parallax1,
     Parallax,
     Conteudo,
     Titulo,
@@ -11,6 +10,7 @@ import {
     Imagem,
     Imagem2,
 } from './styles';
+import { Slide, Zoom } from 'react-reveal';
 
 export default function Home() {
     return (
@@ -18,40 +18,51 @@ export default function Home() {
             <Main>
                 <Parallax style={{ backgroundColor: '#292929' }}>
                     <Imagem>
-                        <Titulo>Raphael de Melo</Titulo>
+                        <Slide left>
+                            <Titulo>Raphael de Melo</Titulo>
+                        </Slide>
                     </Imagem>
                 </Parallax>
 
                 <Parallax>
                     <Conteudo>
-                        <Subtitulo>Qualquer texto aqui...</Subtitulo>
-                        <Texto>
-                            is simply dummy text of the printing and typesetting industry.
-                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                            It has survived not only five centuries, but also the leap into electronic typesetting,
-                            remaining essentially unchanged.
-                            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-                            and more recently with desktop publishing software like Aldus PageMaker
-                            including versions of Lorem Ipsum.
-                        </Texto>
+                        <Slide left>
+                            <Subtitulo>Qualquer texto aqui...</Subtitulo>
+                        </Slide>
 
-                        <Texto>
-                            is simply dummy text of the printing and typesetting industry.
-                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                            It has survived not only five centuries, but also the leap into electronic typesetting,
-                            remaining essentially unchanged.
-                            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-                            and more recently with desktop publishing software like Aldus PageMaker
-                            including versions of Lorem Ipsum.
-                        </Texto>
+                        <Zoom>
+                            <Texto>
+                                is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                                It has survived not only five centuries, but also the leap into electronic typesetting,
+                                remaining essentially unchanged.
+                                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                                and more recently with desktop publishing software like Aldus PageMaker
+                                including versions of Lorem Ipsum.
+                            </Texto>
+                        </Zoom>
+
+                        <Zoom>
+                            <Texto>
+                                is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                                It has survived not only five centuries, but also the leap into electronic typesetting,
+                                remaining essentially unchanged.
+                                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                                and more recently with desktop publishing software like Aldus PageMaker
+                                including versions of Lorem Ipsum.
+                            </Texto>
+                        </Zoom>
                     </Conteudo>
                 </Parallax>
 
                 <Parallax>
                     <Imagem2>
-                        <Titulo>TESTANDO ALGO AQUI</Titulo>
+                        <Slide left>
+                                <Titulo>TESTANDO ALGO AQUI</Titulo>
+                        </Slide>
                     </Imagem2>
                 </Parallax>
             </Main>
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -13,7 +13,7 @@ margin: 0;
 
 export const Parallax = styled.section`
 padding: 40px 0;
-background-color: #ece7d5;
+background-color: #000;
 display: flex;
 flex-direction: column;
 justify-content: center;
@@ -28,6 +28,17 @@ background-size: cover;
 -webkit-background-size: cover;
 -moz-background-size: cover;
 -o-background-size: cover;
+
+:after {
+content: "";
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  z-index: 0;
+  background: -moz-linear-gradient(to top, rgba(0,0,0,0) 0, rgba(0,0,0,0) 45%, #000 100% );
+  background: -webkit-linear-gradient(to top, rgba(0,0,0,0) 0, rgba(0,0,0,0) 45%, #000 100% );
+  background: linear-gradient(to top, rgba(0,0,0,0) 0, rgba(0,0,0,0) 45%, #000 100% );
+}
 `;
 
 export const Titulo = styled.h1`
@@ -40,17 +51,20 @@ width: 100%;
 max-width: 720px;
 margin: 0 auto;
 padding: 0 20px;
+z-index: 99;
 `;
 
 export const Subtitulo = styled.h2`
 color: #fff;
 font-size: 30px;
 margin-bottom: 40px;
+z-index: 99;
 `;
 
 export const Texto = styled.p`
 color: #fff;
 margin-bottom: 40px;
+z-index: 99;
 
 p:last-child{
     margin-bottom: 0;
@@ -72,15 +86,17 @@ display: flex;
 justify-content: center;
 align-items: center;
 
+
+
 :after {
 content: "";
   height: 100%;
   width: 100%;
   position: absolute;
-  z-index: 8;
-  background: -moz-linear-gradient(to bottom, rgba(0,0,0,0) 0, rgba(0,0,0,0) 40%, #000 100% );
-  background: -webkit-linear-gradient(to bottom, rgba(0,0,0,0) 0, rgba(0,0,0,0) 40%, #000 100% );
-  background: linear-gradient(to bottom, rgba(0,0,0,0) 0, rgba(0,0,0,0) 40%, #000 100% );
+  z-index: 0;
+  background: -moz-linear-gradient(to bottom, rgba(0,0,0,0) 0, rgba(0,0,0,0) 60%, #000 100% );
+  background: -webkit-linear-gradient(to bottom, rgba(0,0,0,0) 0, rgba(0,0,0,0) 60%, #000 100% );
+  background: linear-gradient(to bottom, rgba(0,0,0,0) 0, rgba(0,0,0,0) 60%, #000 100% );
 }
 `;
 
@@ -104,7 +120,7 @@ content: "";
   height: 100%;
   width: 100%;
   position: absolute;
-  z-index: 8;
+  z-index: 0;
   background: -moz-linear-gradient(to bottom, rgba(0,0,0,0) 0, rgba(0,0,0,0) 40%, #000 100% );
   background: -webkit-linear-gradient(to bottom, rgba(0,0,0,0) 0, rgba(0,0,0,0) 40%, #000 100% );
   background: linear-gradient(to bottom, rgba(0,0,0,0) 0, rgba(0,0,0,0) 40%, #000 100% );
