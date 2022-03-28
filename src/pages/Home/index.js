d940b26ecdff8c9b4133e3d9b7fd85b856ddf0ff
Raphael de Melo <raphaeldemelosoares@hotmail.com>
2022-03-28 11:36:16 -0300

    modificando a home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,77 +1,46 @@
 import React from 'react';
 import {
     Container,
-    Main,
-    Parallax,
     Conteudo,
-    Titulo,
-    Subtitulo,
-    Texto,
+    Card,
+    CardConteudo,
     Imagem,
-    Imagem2,
 } from './styles';
 import { Slide, Zoom } from 'react-reveal';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
+import Apresentacao from '../../components/Apresentacao';
+import FotoPerfil from '../../assets/FotoPerfil.jpg';
 
 export default function Home() {
     return (
         <Container>
-            <Header/>
-            <Main>
-                <Parallax style={{ backgroundColor: '#292929' }}>
-                    <Imagem>
+            <Header />
+
+            <Conteudo style={{
+                backgroundImage: `url(https://images.unsplash.com/photo-1555066931-4365d14bab8c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80)`,
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+            }}>
+                <Card>
+                    <CardConteudo>
+                        <Apresentacao />
+                    </CardConteudo>
+
+                    <CardConteudo style={{ paddingLeft: '200px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                         <Slide top>
-                            <Titulo style={{ fontWeight: 'lighter' }}>Oi, eu sou Raphael</Titulo>
+                            <Imagem
+                                src={FotoPerfil}
+                            />
                         </Slide>
-                        <Slide bottom>
-                            <Titulo style={{ color: 'red' }}>Desenvolvedor Front-end</Titulo>
-                        </Slide>
-                    </Imagem>
-                </Parallax>
+                    </CardConteudo>
+                </Card>
+            </Conteudo>
 
-                <Parallax>
-                    <Conteudo>
-                        <Slide left>
-                            <Subtitulo>Qualquer texto aqui...</Subtitulo>
-                        </Slide>
+            <Conteudo>
 
-                        <Zoom>
-                            <Texto>
-                                is simply dummy text of the printing and typesetting industry.
-                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                                It has survived not only five centuries, but also the leap into electronic typesetting,
-                                remaining essentially unchanged.
-                                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-                                and more recently with desktop publishing software like Aldus PageMaker
-                                including versions of Lorem Ipsum.
-                            </Texto>
-                        </Zoom>
+            </Conteudo>
 
-                        <Zoom>
-                            <Texto>
-                                is simply dummy text of the printing and typesetting industry.
-                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                                It has survived not only five centuries, but also the leap into electronic typesetting,
-                                remaining essentially unchanged.
-                                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-                                and more recently with desktop publishing software like Aldus PageMaker
-                                including versions of Lorem Ipsum.
-                            </Texto>
-                        </Zoom>
-                    </Conteudo>
-                </Parallax>
-
-                <Parallax>
-                    <Imagem2>
-                        <Slide duration="2000" left>
-                            <Titulo>TESTANDO ALGO AQUI</Titulo>
-                        </Slide>
-                    </Imagem2>
-                </Parallax>
-            </Main>
             <Footer />
         </Container >
     );
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,130 +1,33 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-width: 100%;
-margin: 0 auto;
-`;
-
-
-export const Main = styled.div`
-display: block;
-margin: 0;
-`;
-
-export const Parallax = styled.section`
-padding: 40px 0;
-background-color: #000;
-display: flex;
-flex-direction: column;
-justify-content: center;
-height: 100vh !important;
-width: 100%;
-position: relative;
-overflow: hidden;
-background-position: 50% 50%;
-background-repeat: no-repeat;
-background-attachment: fixed;
-background-size: cover;
--webkit-background-size: cover;
--moz-background-size: cover;
--o-background-size: cover;
-
-:after {
-content: "";
-  height: 100%;
-  width: 100%;
-  position: absolute;
-  z-index: 0;
-  background: -moz-linear-gradient(to top, rgba(0,0,0,0) 0, rgba(0,0,0,0) 45%, #000 100% );
-  background: -webkit-linear-gradient(to top, rgba(0,0,0,0) 0, rgba(0,0,0,0) 45%, #000 100% );
-  background: linear-gradient(to top, rgba(0,0,0,0) 0, rgba(0,0,0,0) 45%, #000 100% );
-}
-`;
-
-export const Titulo = styled.h1`
-color: #Fff;
-font-size: 50px;
+height: 100%;
 `;
 
 export const Conteudo = styled.section`
 width: 100%;
-max-width: 720px;
-margin: 0 auto;
-padding: 0 20px;
-z-index: 99;
-`;
-
-export const Subtitulo = styled.h2`
-color: #fff;
-font-size: 30px;
-margin-bottom: 40px;
-z-index: 99;
-`;
-
-export const Texto = styled.p`
-color: #fff;
-margin-bottom: 40px;
-z-index: 99;
-
-p:last-child{
-    margin-bottom: 0;
-}
-`;
-
-export const Imagem = styled.image`
-background-image: url("https://images.unsplash.com/photo-1555066931-4365d14bab8c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80");
-width: 100%;
 height: 100%;
-background-position: 50% 50%;
-background-repeat: no-repeat;
-background-attachment: fixed;
-background-size: cover;
--webkit-background-size: cover;
--moz-background-size: cover;
--o-background-size: cover;
 display: flex;
-flex-direction: column;
-justify-content: center;
 align-items: center;
+justify-content: center;
 
-
-
-:after {
-content: "";
-  height: 100%;
-  width: 100%;
-  position: absolute;
-  z-index: 0;
-  background: -moz-linear-gradient(to bottom, rgba(0,0,0,0) 0, rgba(0,0,0,0) 60%, #000 100% );
-  background: -webkit-linear-gradient(to bottom, rgba(0,0,0,0) 0, rgba(0,0,0,0) 60%, #000 100% );
-  background: linear-gradient(to bottom, rgba(0,0,0,0) 0, rgba(0,0,0,0) 60%, #000 100% );
-}
 `;
 
-export const Imagem2 = styled.image`
-background-image: url("https://cdn.pixabay.com/photo/2020/05/07/04/01/digitization-5140071_960_720.jpg");
-width: 100%;
+export const Card = styled.div`
+background-color: rgba(37, 37, 37, 0.8);
+width: 90%;
+height: 70%;
+border-radius: 10px;
+display: flex;
+flex-direction: row;
+`;
+
+export const CardConteudo = styled.div`
 height: 100%;
-background-position: 50% 50%;
-background-repeat: no-repeat;
-background-attachment: fixed;
-background-size: cover;
--webkit-background-size: cover;
--moz-background-size: cover;
--o-background-size: cover;
-display: flex;
-justify-content: center;
-align-items: center;
-
-:after {
-content: "";
-  height: 100%;
-  width: 100%;
-  position: absolute;
-  z-index: 0;
-  background: -moz-linear-gradient(to bottom, rgba(0,0,0,0) 0, rgba(0,0,0,0) 40%, #000 100% );
-  background: -webkit-linear-gradient(to bottom, rgba(0,0,0,0) 0, rgba(0,0,0,0) 40%, #000 100% );
-  background: linear-gradient(to bottom, rgba(0,0,0,0) 0, rgba(0,0,0,0) 40%, #000 100% );
-}
+width: 60%;
 `;
 
+export const Imagem = styled.img`
+width: 70%;
+border: 10px solid white;
+`;
