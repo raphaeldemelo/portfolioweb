c1be73c8f7b1368a2428552ef1bc3409771af69f
Raphael de Melo <raphaeldemelosoares@hotmail.com>
2022-03-28 11:35:51 -0300

    criando apresentação

---
+++ src/components/Apresentacao/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles.css';
+
+export default function Apresentacao() {
+    return (
+        <div>
+            <div>
+                <h2>
+                    Raphael de Melo
+                    <span>Raphael de Melo</span>
+                    <span>Raphael de Melo</span>
+                    <span>Desenvolvedor Front-End</span>
+                </h2>
+            </div>
+        </div>
+    );
+}
